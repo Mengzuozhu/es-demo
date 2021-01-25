5b7965381c19256af440816def68d5b39cb99600
zuozhu.meng <zuozhu.meng@yitu-inc.com>
2021-01-25 20:05:57 +0800

    change AggregationServiceTest

--- src/test/java/com/mzz/esdemo/service/AggregationServiceTest.java
+++ src/test/java/com/mzz/esdemo/service/AggregationServiceTest.java
@@ -1,49 +1,82 @@
 package com.mzz.esdemo.service;
 
-import com.mzz.esdemo.EsDemoApplication;
+import com.mzz.esdemo.common.TestElasticsearchHandler;
 import com.mzz.esdemo.common.constant.EsConstant;
+import com.mzz.esdemo.model.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Comparator;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author zuozhu.meng
  * @since 2020/10/21
  **/
-@SpringBootTest(classes = EsDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AggregationServiceTest {
-    @Autowired
-    private AggregationService aggregationService;
+    private static TestElasticsearchHandler elasticsearchHandler = new TestElasticsearchHandler();
+    private AggregationService aggregationService =
+            new AggregationService(TestElasticsearchHandler.restHighLevelClient());
+
+    @AfterAll
+    static void afterAll() {
+        elasticsearchHandler.clearIndex();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        elasticsearchHandler.upsertDoc();
+    }
 
     @Test
     void distinctCount() {
         long distinctCount = aggregationService.distinctCount(EsConstant.INDEX_NAME, "degree.keyword");
-        System.out.println(distinctCount);
+        assertEquals(getDegreeCount(), distinctCount);
     }
 
     @Test
     void minAgg() {
-        double maxAgg = aggregationService.minAgg(EsConstant.INDEX_NAME, "height");
-        System.out.println(maxAgg);
+        double minAgg = aggregationService.minAgg(EsConstant.INDEX_NAME, "height");
+        Optional<Integer> optional = getHeightStream().min(Comparator.naturalOrder());
+        optional.ifPresent(val -> assertEquals(val, (int) minAgg));
     }
 
     @Test
     void maxAgg() {
         double maxAgg = aggregationService.maxAgg(EsConstant.INDEX_NAME, "height");
-        System.out.println(maxAgg);
+        Optional<Integer> optional = getHeightStream().max(Comparator.naturalOrder());
+        optional.ifPresent(val -> assertEquals(val, (int) maxAgg));
     }
 
     @Test
     void avgAgg() {
-        double maxAgg = aggregationService.avgAgg(EsConstant.INDEX_NAME, "height");
-        System.out.println(maxAgg);
+        double avgAgg = aggregationService.avgAgg(EsConstant.INDEX_NAME, "height");
+        System.out.println(avgAgg);
+        assertTrue(avgAgg > 0);
     }
 
     @Test
     void termsAgg() {
         Map<Object, Long> termsAgg = aggregationService.termsCountAgg(EsConstant.INDEX_NAME, "degree.keyword");
-        System.out.println(termsAgg.toString());
+        assertEquals(getDegreeCount(), termsAgg.size());
+    }
+
+    private Stream<Integer> getHeightStream() {
+        return TestElasticsearchHandler.getUsers()
+                .stream()
+                .map(User::getHeight);
+    }
+
+    private long getDegreeCount() {
+        return TestElasticsearchHandler.getUsers()
+                .stream()
+                .map(User::getDegree)
+                .distinct()
+                .count();
     }
 }
